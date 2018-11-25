aacffe4f3718919881f03bde06eea78b03abf097
Stephen Karger <stephen.karger@gmail.com>
2018-11-25 12:54:15 -0500

    Align elements of contact page

--- server.rb
+++ server.rb
@@ -18,16 +18,21 @@ class Server < Roda
   ADDRESS_LOCALITY = 'Stamford'
   ADDRESS_REGION = 'CT'
   POSTAL_CODE = '06905'
+  GOOGLE_MAPS_LINK = 'https://www.google.com/maps/place/104+Newfield+Dr,+Stamford,+CT+06905/@41.1084539,-73.5362619'
 
-  LAYOUT_LOCALS = {
-    name: NAME,
-    credentials: CREDENTIALS,
+  CONTACT_INFO = {
     telephone: TELEPHONE,
     street_address: STREET_ADDRESS,
     address_locality: ADDRESS_LOCALITY,
     address_region: ADDRESS_REGION,
-    postal_code: POSTAL_CODE
+    postal_code: POSTAL_CODE,
+    google_maps_link: GOOGLE_MAPS_LINK
   }.freeze
+
+  LAYOUT_LOCALS = {
+    name: NAME,
+    credentials: CREDENTIALS
+  }.merge(CONTACT_INFO).freeze
 
   GOOGLE_MAPS_QUERY = '104+Newfield+Drive,Stamford+CT+06905'
   GOOGLE_MAPS_EMBED_URL = 'https://www.google.com/maps/embed/v1/place' \
@@ -37,9 +42,10 @@ class Server < Roda
     '&attribution_source=Google+Maps+Embed+API' \
     "&attribution_web_url=#{ENV['PUBLIC_URL']}" \
     "attribution_ios_deep_link_id=comgooglemaps://?daddr=#{GOOGLE_MAPS_QUERY}"
+
   CONTACT_PAGE_LOCALS = {
     google_maps_embed_url: GOOGLE_MAPS_EMBED_URL
-  }.freeze
+  }.merge(CONTACT_INFO).freeze
 
   def layout_locals(request)
     LAYOUT_LOCALS.merge(current_path: request.path)

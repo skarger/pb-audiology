1da8fc7205b6825e0c5aea0ff12c0069000d54ba
Stephen Karger <stephen.karger@gmail.com>
2019-03-10 20:53:03 -0400

    Use fonts

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,10 @@ module.exports = {
             loader: 'postcss-loader'
           }
         ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },

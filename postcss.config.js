b591b65eb1face7f56a9ab52db09830db5a73881
Stephen Karger <stephen.karger@gmail.com>
2020-01-09 23:24:54 -0500

    Package upgrades (#7)
    Most notably, upgraded tailwindcss to 1.0.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-var tailwindcss = require('tailwindcss');
-
 module.exports = ({ file, options, env }) => ({
   plugins: {
-    'tailwindcss': './tailwind.js',
+    'tailwindcss': {},
     'autoprefixer': { grid: true },
     'cssnano': env === 'production'
   }
